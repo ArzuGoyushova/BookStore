279d8073f88a259339d8a8e2a18fdad0a5e0a117
ArzuGoyushova <arzusg@code.edu.az>
2023-09-15 21:31:41 +0400

    refactor: Book Store Project is updated.

---
+++ server/BookStoreAPI/src/Core/BookStoreAPI.Domain/Entities/BookGenre.cs
@@ -0,0 +1,13 @@
+﻿using BookStoreAPI.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreAPI.Domain.Entities
+{
+    public class BookGenre : BaseEntity
+    {
+    }
+}
