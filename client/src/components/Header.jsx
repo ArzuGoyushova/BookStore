fd65905f61cc41e384a57e0e513bff15b28fee10
ArzuGoyushova <arzusg@code.edu.az>
2023-09-29 20:47:37 +0400

    refactor: Book Store Project is updated.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { navLinks } from '../constants/constant';
+import {CiSearch, CiBookmark} from 'react-icons/ci';
+import {LiaShoppingBagSolid} from 'react-icons/lia';
+import {BiUser} from 'react-icons/bi';
 
 const Header = () => {
   return (
     <header className='container mx-auto px-10'>
-    <nav className='flex justify-between items-center border'>
+    <nav className='flex justify-between items-center border h-16'>
       <div className=''>
         <img src='../public/images/logo.png'/>
       </div>
@@ -15,6 +18,22 @@ const Header = () => {
             {link.title}
           </li>
         ))}
+      </ul>
+      </div>
+       <div className='flex justify-around items-center'>
+      <ul className="list-unstyled flex items-center flex-grow-1">
+      <li className="me-4">
+          <CiSearch/>
+          </li>
+          <li className="me-4">
+          <CiBookmark/>
+          </li>
+          <li className="me-4">
+          <LiaShoppingBagSolid/>
+          </li>
+            <li className="me-4">
+          <BiUser/>
+          </li>
       </ul>
       </div>
     </nav>
