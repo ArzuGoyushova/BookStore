65fd1bd33a4edce52710ef04e1b6e4327c88599c
ArzuGoyushova <arzusg@code.edu.az>
2023-12-20 14:44:57 +0400

    updated

--- src/components/main/checkout/CheckoutShoppingBag.jsx
+++ src/components/main/checkout/CheckoutShoppingBag.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { BiX } from 'react-icons/bi';
+import { Link } from 'react-router-dom';
 
 const CheckoutShoppingBag = () => {
     const [cart, setCart] = useState([]);
@@ -44,7 +45,10 @@ const CheckoutShoppingBag = () => {
     return (
       <div className="md:ms-20">
         <div>
+          <div className='flex justify-between items-center'>
             <span className='text-2xl hidden md:flex'>Shopping Bag</span>
+            <Link className='underline text-blue-800 text-lg' to="/basket">Edit Bag</Link>
+            </div>
           {cart.map((item) => (
             <div key={item.id} className="md:flex justify-between my-2 border-b-2 pb-2 space-y-4">
               <div className='md:w-1/5 w-full'>
@@ -54,11 +58,9 @@ const CheckoutShoppingBag = () => {
                 <h3 className="md:text-xl text-m font-bold">{item.title}</h3>
                 <p className="text-gray-500">{item.author}</p>
               </div>
-              <div className='flex justify-between items-center border p-2 md:border-0 md:p-0 md:block'>
-              <div className='md:w-1/5 font-bold'>
-                ${item.regularPrice}
-              </div>
-              <div className='md:w-1/5'>
+              <div className='md:w-2/5 flex justify-between items-start border p-2 md:border-0 md:p-0'>
+           
+              <div className='md:w-1/3 md:flex'>
                 <button
                   className="font-bold"
                   onClick={() => {
@@ -69,13 +71,16 @@ const CheckoutShoppingBag = () => {
                 >
                   -
                 </button>
-                <span className="border border-gray-400 px-3 py-1 m-2">{item.quantity}</span>
+                <span className="border border-gray-400 px-3 mx-2">{item.quantity}</span>
                 <button
                   className="font-bold"
                   onClick={() => updateQuantity(item.id, item.quantity + 1)}
                 >
                   +
                 </button>
+              </div>
+              <div className='md:w-1/3 font-bold'>
+                ${item.regularPrice}
               </div>
               <button onClick={() => removeFromCart(item.id)} className="flex items-start text-gray-800">
                 <BiX className='w-6 h-6' />
@@ -88,10 +93,10 @@ const CheckoutShoppingBag = () => {
             <input
             type='text'
             placeholder='Discount code'
-            className='p-2 border border-gray-400 w-2/3 md:w-4/5'
+            className='p-2 border border-gray-400 w-2/3 md:w-3/5'
             />
           <button
-            className=" w-1/3 md:w-1/5 bg-orange-600 hover:bg-orange-700 text-white py-2 px-4 md:px-12 text-m"
+            className="w-1/3 md:w-2/5 bg-orange-600 hover:bg-orange-700 text-white py-2 px-4 md:px-12 text-m"
           >
             <span>Apply</span>
           </button>
